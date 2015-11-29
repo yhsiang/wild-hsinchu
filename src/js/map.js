667eb04f574ce4aecf1d618f58283a13d2fd38d5
Ly Cheng <lyforever62@hotmail.com>
2015-11-29 11:11:26 +0800

    Add road api and style

--- src/js/map.js
+++ src/js/map.js
@@ -2,9 +2,30 @@ L.mapbox.accessToken = 'pk.eyJ1IjoieWhzaWFuZyIsImEiOiJjaWhqMGw3ZncwNWVydGpqN2l2a
 var map = L.mapbox.map('map', 'mapbox.comic')
     .setView([24.8047222, 120.9713889], 15);
 var features = [];
-var data = [{"department":"台灣電力公司","location":"湳雅街313巷68號前 ","coordx":"120.9761284942","coordy":"24.8245604370561","desc":"電力電纜管路遷移","reasonCode":"06","startDate":"1041124","finishDate":"1041217","pipeType":"埋設地下管路,配合用戶線路遷移"},{"department":"台灣中油股份有限公司天燃氣事業部北區營業處","location":"美森街8巷5號、7號","coordx":"120.925164125133","coordy":"24.7920276201802","desc":"緊急搶修","reasonCode":"02","startDate":"1041126","finishDate":"1041128","pipeType":"天然氣管線"},{"department":"新竹瓦斯股份有限公司新竹服務中心","location":"光華二街７２巷４０弄２-1號至46號","coordx":"120.978993536079","coordy":"24.8177188142345","desc":"瓦斯搶修(零星)","reasonCode":"02","startDate":"1041127","finishDate":"1041130","pipeType":"瓦斯管線"}];
+// var data = [{"department":"台灣電力公司","location":"湳雅街313巷68號前 ","coordx":"120.9761284942","coordy":"24.8245604370561","desc":"電力電纜管路遷移","reasonCode":"06","startDate":"1041124","finishDate":"1041217","pipeType":"埋設地下管路,配合用戶線路遷移"},{"department":"台灣中油股份有限公司天燃氣事業部北區營業處","location":"美森街8巷5號、7號","coordx":"120.925164125133","coordy":"24.7920276201802","desc":"緊急搶修","reasonCode":"02","startDate":"1041126","finishDate":"1041128","pipeType":"天然氣管線"},{"department":"新竹瓦斯股份有限公司新竹服務中心","location":"光華二街７２巷４０弄２-1號至46號","coordx":"120.978993536079","coordy":"24.8177188142345","desc":"瓦斯搶修(零星)","reasonCode":"02","startDate":"1041127","finishDate":"1041130","pipeType":"瓦斯管線"}];
 var markerList = document.getElementById('marker-list');
-
+var icons = {
+  "warn": {
+    "iconUrl": "/images/contruction/2.png",
+    "iconSize": [32, 32],
+    "className": "dot"
+  },
+  "pipe": {
+    "iconUrl": "/images/contruction/1.png",
+    "iconSize": [32, 32],
+    "className": "dot"
+  },
+  "danger": {
+    "iconUrl": "/images/contruction/4.png",
+    "iconSize": [32, 32],
+    "className": "dot"
+  },
+  "cover": {
+    "iconUrl": "/images/contruction/3.png",
+    "iconSize": [32, 32],
+    "className": "dot"
+  }
+};
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var pos = [
@@ -28,7 +49,10 @@ if (navigator.geolocation) {
         }
       });
       map.setView(pos, 15);
-      generate(data);
+      $.ajax('https://hsinchu.herokuapp.com/road')
+        .then(function(res) {
+          generate(res.results);
+        });
     }, function() {
       alert('你的瀏覽器不支援 Geolocation');
     });
@@ -42,6 +66,18 @@ function generate(data) {
 
 
   data.forEach(function(it) {
+    var icon = icons['warn'];
+
+    if (it.pipeType.match(/自來水|污水/)) {
+      icon = icons['pipe'];
+    }
+    if (it.pipeType.match(/埋設|遷移/)) {
+      icon = icons['danger'];
+    }
+    if (it.pipeType.match(/手孔|孔蓋/)) {
+      icon = icons['cover'];
+    }
+
     features.push({
       type: 'Feature',
       geometry: {
@@ -51,11 +87,9 @@ function generate(data) {
         ]
       },
       properties: {
-        title: it.department,
-        description: it.location + " " + it.desc,
-        'marker-size': 'large',
-        'marker-color': '#BE9A6B',
-        'marker-symbol': 'cafe'
+        title:  it.desc,
+        description: it.location,
+        icon: icon
       }
     });
   });
@@ -63,12 +97,23 @@ function generate(data) {
   var mylayer = L.mapbox.featureLayer(features).addTo(map);
 
   mylayer.eachLayer(function(layer) {
+    var properties = layer.toGeoJSON().properties;
      var item = markerList.appendChild(document.createElement('li'));
-     item.innerHTML = layer.toGeoJSON().properties.title;
+
+
      item.onclick = function() {
         map.setView(layer.getLatLng(), 15);
         layer.openPopup();
      };
-     if (layer.toGeoJSON().properties.title === '您所在的位置') layer.openPopup();
+     if (properties.title === '您所在的位置') {
+       item.innerHTML = properties.title;
+       layer.openPopup();
+     } else {
+       var img = item.appendChild(document.createElement('img'));
+       img.src = properties.icon.iconUrl;
+       var text = item.appendChild(document.createElement('span'));
+       text.innerHTML = properties.description;
+       layer.setIcon(L.icon(properties.icon));
+     }
   });
 }
